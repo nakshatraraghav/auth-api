9fc3fc04b0ae77c04af632ba9371e66895c2126f
nakshatra raghav <combatrickshaw@gmail.com>
2023-05-03 12:21:53 +0530

    session model

---
+++ src/models/session.model.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+interface sessionDocument {
+  user: mongoose.Types.ObjectId;
+  valid: boolean;
+  userAgent: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const sessionSchema = new mongoose.Schema<sessionDocument>({
+  user: {
+    required: true,
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "users",
+  },
+  valid: {
+    type: Boolean,
+    default: true,
+  },
+  userAgent: {
+    required: true,
+    type: String,
+  },
+});
+
+const sessionModel = mongoose.model("sessions", sessionSchema);
+export default sessionModel;
