e0b77de2817edac46eac8a1abd201028b10750b8
nakshatra raghav <combatrickshaw@gmail.com>
2023-05-03 10:01:04 +0530

    user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+interface userDocument {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+const userSchema = new mongoose.Schema<userDocument>(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    email: {
+      required: true,
+      unique: true,
+      type: String,
+    },
+    password: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("users", userSchema);
+export default UserModel;
